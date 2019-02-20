b67df3ed9fa3338549f73be44336526e6f263eb8
nimu <niko.muukkonen@protacon.com>
2019-02-20 08:22:55 +0200

    Initial commit

---
+++ EuroJackpotApp/EuroJackpotApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EuroJackpotApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
